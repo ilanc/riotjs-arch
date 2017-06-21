26fb89d713defa5424b346b66d8aaf45c1cf3f24
Ilan Copelyn <ilan@netreturn.co.za>
2017-06-21 03:22:35 +0200

    progress bar

---
+++ src/progress-bar/webpack.config.js
@@ -0,0 +1,46 @@
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	target: 'web',
+	devtool: 'inline-source-map',
+
+	resolve: {
+		extensions: [
+			".js"
+		]
+	    },
+	entry: {
+		app: [
+			"./src/progress-bar/app.js"
+		]
+	},
+	output: {
+		path: path.join(__dirname, '/../../dist/progress-bar/'),
+		filename: "[name].min.js"
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: [
+					{ loader: 'style-loader', options: { base: 2000 } },
+					'css-loader'
+				]
+			},
+			{ test: /\.(png|jpg|jpeg|gif|svg)$/, loader: 'file-loader', options: { name: 'img/[name].[ext]' } },
+			//{ test: /\.(png|jpg|jpeg|gif|svg)$/, loader: 'url-loader', options: { limit: 100000 } },
+			{ test: /\.tag(.html)?$/, exclude: /node_modules/, loader: 'riot-tag-loader', options: { type: 'es6', /*hot: true,*/ debug: true } }, // transpile the riot tags using babel
+			{ test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
+		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			inject: 'body',
+			hash: true,
+			template: './src/progress-bar/index.html',
+			filename: 'index.html'
+		})
+	]
+};
