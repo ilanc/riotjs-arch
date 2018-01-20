a936cbb459afafe6ae27151f1723b7b421a386bf
Ilan Copelyn <ilan@netreturn.co.za>
2018-01-20 15:39:31 +0200

    router

---
+++ src/router/app.js
@@ -0,0 +1,13 @@
+console.log('app.js');
+// Undo "prevent flash of unstyled content" - see <body style="display:none;"> in index.html
+// NOTE: if there are .js errors then <body> is not displayed - undo below
+//document.getElementsByTagName('body')[0].style.display = "";
+
+// deps
+import riot from 'riot'
+import riotroute from 'riot-route'
+import $ from 'jquery'
+import './app.tag.html'
+
+riot.mount('app')
+$('body').show();
